907463f454bb63806e03a30cfd8e8522417c8a6f
frenkydwi-blip <frenky.dwi@gmail.com>
2026-09-18 14:09:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.WIUP_CONFIG = {
   // Isi dua nilai berikut setelah membuat project Supabase.
   // Jika dibiarkan kosong, dashboard tetap berjalan dalam mode preview menggunakan data spreadsheet.
-  SUPABASE_URL: "https://gbrdqimtpmbswluzyerd.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_JwebePX9Veu00q4yGvB3XA_2xNPKRd2",
+  SUPABASE_URL: "https://afavjezuhvwjlmtcjkvg.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ROjnuAbdLpiFi9_oermFmw_4oQzw4Lq",
   PROJECT_ID: 1
 };
